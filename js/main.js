dc2a6153bac6b28975178f3ce4078a9d8419b95d
Michele Zucca <michele.zucca81@gmail.com>
2020-09-02 11:02:15 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// scrollreveal
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.tagline', { delay: 500 })
+ScrollReveal().reveal('.punchline', { delay: 1000})
+ScrollReveal().reveal('.punchline2', { delay: 2000})
+ScrollReveal().reveal('.punchline3', { delay: 2500})
